14be66b2a74074557003deab73b1ffaeb59f5620
VINICIUS SCHÜRHAUS DO NASCIMENTO <vinicius_nascimen5@estudante.sesisenai.org.br>
2025-05-13 15:37:55 -0300

    Configuração básica do servidor Express

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function registrarLog(nomeAluno) {
 
     return id;
 }
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
